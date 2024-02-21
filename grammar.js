95e416b4a4f64debe702d4bf8dc3695cd7204f3e
J Lorieau <justin@lorieau.com>
2024-02-21 11:12:40 -0600

    Add optional tag attributes

--- grammar.js
+++ grammar.js
@@ -7,89 +7,52 @@ module.exports = grammar({
   rules: {
     document: $ => repeat($.p),
 
+    // A paragraph containing one or more nodes and a new block 
+    // (an extra newline or end-of-file)
     p: $ => choice(
       seq(repeat1($._node), $._new_block),
       seq(repeat1($._node), EOF),
     ),
     
+    // A (hidden) node containing either text or a tag.
     _node: $ => choice($.text, $.tag),
   
-    // A tag with contents
+    // A tag with contents comprising a node
     tag: $ => seq(
       TAG_IDENT,
       $.tag_name,
+      optional($.attributes),  // optional attributes
       "{",
       repeat($._node),
       "}"
-  ),
+    ),
 
-    // A tag identifier with a tag name and (optional) tag attributes
-    // _start_tag: $ => seq(
-    //   TAG_IDENT,
-    //   $.tag_name,
-    //   // optional($.attributes),
-    // ),
+    // Tag attributes
+    attributes: $ => seq(
+      "[",
+      repeat($.attribute),
+      "]",
+    ),
 
-    // text: _ => /([^\n]+(\n)?)+/,  // Block of text
+    attribute: $ => seq(
+      $.attribute_name,
+      optional(seq(
+        '=',
+        choice(
+          $.attribute_value,
+          $.quoted_attribute_value,
+        ),
+      )),
+    ),
+
     text: _ => /([^\n{}@]+(\n)?)+/,  // Block of text
     _new_block: _ => /[\n\s*]+/,  // New block
     tag_name: _ => /[a-zA-Z][\w_]*/,  // Allowable tag names
-
-
-    //
-
-    // node: $ => choice(
-    //   // $.tag,
-    //   field('text', $.text),
-    // ),
-    
-    // // A tag with contents
-    // tag: $ => seq(
-    //     $._start_tag,
-    //     "{",
-    //     repeat($.node),
-    //     "}"
-    // ),
-
-    // // A tag identifier with a tag name and (optional) tag attributes
-    // _start_tag: $ => seq(
-    //   TAG_IDENT,
-    //   field('name', $.tag_name),
-    //   optional($.attributes),
-    // ),
-
-    // // Allowable tag names
-    // tag_name: _ => /[a-zA-Z][\w_]*/,
-
-    // // Attributes
-    // attributes: $ => seq(
-    //   "[",
-    //   repeat($.attribute),
-    //   "]",
-    // ),
-
-    // attribute: $ => seq(
-    //   $.attribute_name,
-    //   optional(seq(
-    //     '=',
-    //     choice(
-    //       $.attribute_value,
-    //       $.quoted_attribute_value,
-    //     ),
-    //   )),
-    // ),
-
-    // attribute_name: _ => /[^{}"'/=\s\]]+/,
-
-    // attribute_value: _ => /[^{}"'=\s\]]+/,
-
-    // quoted_attribute_value: $ => choice(
-    //   seq('\'', optional(alias(/[^']+/, $.attribute_value)), '\''),
-    //   seq('"', optional(alias(/[^"]+/, $.attribute_value)), '"'),
-    // ),
-
-    // // Regular text without tags
-    // text: _ => /([^\n]+\n)+/,
-    // _nl: _ => /\n/,
+    attribute_name: _ => /[^{}"'/=\s\]]+/,  // Name of attribute
+    attribute_value: _ => /[^{}"'=\s\]]+/,  // Value of attribute
+    quoted_attribute_value: $ => choice(
+        seq('\'', optional(alias(/[^']+/, $.attribute_value)), '\''),
+        seq('"', optional(alias(/[^"]+/, $.attribute_value)), '"'),
+      ),
   }
 });
